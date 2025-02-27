416bb758a982c8e0ab3d0c3aae0a93fafd4a617b
SKarthikeyan06 <karthikeyansureshbabu06@gmail.com>
2025-02-27 21:38:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function checkTime(targetTime1, targetTime2) {
     return false;
   }
 const targetTime1 = "10:30:00";
-const targetTime2 = "10:25:00";
+const targetTime2 = "21:40:00";
 setInterval(() => {
     if (checkTime(targetTime1, targetTime2)) {
         call_sms()
